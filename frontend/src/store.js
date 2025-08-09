d2c4be59c73e5aa3cde263f1d1429671f8de3662
YJH <ybox92@gmail.com>
2025-08-10 04:07:18 +0900

    [REFACTOR] store.js import 정리

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import AnalyticsSlice from './slices/AnalyticsSlice';
 import VisitorCountSlice from './slices/VisitorCountSlice';
 import OrderCountSlice from './slices/OrderCountSlice';
+import UserCountSlice from './slices/UserCountSlice';
 
 const store = configureStore({
   reducer: {
     AnalyticsSlice,
     VisitorCountSlice,
-    OrderCountSlice
+    OrderCountSlice,
+    UserCountSlice
   }
 });
 
